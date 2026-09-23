00832e34fd64ffaf03bd7a85b322bcd4a8343752
exemptus9 <idunnopoetry@gmail.com>
2026-09-23 16:09:42 -0400

    Correct museum release version to v3.9

--- museum-static/museum-config.js
+++ museum-static/museum-config.js
@@ -1,1 +1,1 @@
-window.IDP_CONFIG={version:'3.5',releaseName:'Social Preview',releaseDate:'2026-09-23',archiveOwner:'Brandon WordSmith',contactLabel:'Recovery / archive contact',contactUrl:'https://rhymemosaic.com',siteLabel:'RhymeMosaic'};
+window.IDP_CONFIG={version:'3.9',releaseDate:'2026-09-23',archiveOwner:'Brandon WordSmith',contactLabel:'Recovery / archive contact',contactUrl:'https://rhymemosaic.com',siteLabel:'RhymeMosaic'};
